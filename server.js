8f5c2efe8dfd35166af0ce22965407d6d45eba5f
Sergey Karakash <karakash.sergey@gmail.com>
2019-11-12 16:06:27 +0200

    added sequelize & model

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const Match = require('./Models/Match');
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 
 app.get(`/`, (req,res) => {
-	res.send('Works');
-})
+	res.send('Home');
+});
 
 
 app.listen(port, ()=> console.log(`running on ${port}`));
