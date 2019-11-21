05e04341ee09767aea8c9b3d119e4926ef420b05
Sergey Karakash <karakash.sergey@gmail.com>
2019-11-21 17:44:59 +0200

    added time display for inplay

--- client/src/Components/Inplay.js
+++ client/src/Components/Inplay.js
@@ -23,7 +23,7 @@ class Inplay extends Component {
 
     getGames = () => getAllById(this.state.ids).then(
         data => {
-            const matches = data.filter(game => Number(game.timer.tm) >= 45).map(game => getGameData(game));
+            const matches = data.filter(game => Number(game.time_status === 3)).map(game => getGameData(game));
             this.setState({
                 matches
             }, () => console.log(this.state.matches))
@@ -33,10 +33,9 @@ class Inplay extends Component {
     render(){
         const { ids, matches } = this.state;
         return <div className="container">
-            {ids.length > 0 && ids.map(id => <div key={id}>{id}</div>)}
             <button className="btn btn-success" onClick={() => this.getIds()}>LIVE</button>
             <button className="btn btn-info" onClick={() => this.getGames()}>Display live</button>
-            { matches.length > 0 && matches.map(match => <div key={match.teams}>{match.teams} {match['HT']}   {match['@35']}  {match['@40']}  {match['@45']}  {match['@50'] || 0}  {match['@55'] || 0} TIME: {match.time}</div>) }
+            { matches.length > 0 && matches.map(match => <div key={match.teams}>{match.teams} {match['HT']}   {match['@35']}  {match['@40']}  {match['@45']}  {match['@50'] || 0}  {match['@55'] || 0} FT: {match['FT'] || 0} --- TIME: {match.time}</div>) }
         </div>
     }
 }
