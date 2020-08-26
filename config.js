b64e04f82a1534e5fdb7d5b70660059fbf5d97cf
Sergey Karakash <karakash.sergey@gmail.com>
2020-08-26 13:10:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     token: '*****',
-    mongo_uri: 'mongodb+srv://serg_ka:highbury@apuestas-ysqq3.mongodb.net/Stats?retryWrites=true&w=majority'
+    mongo_uri: 'mongodb+srv://serg_ka:******@apuestas-ysqq3.mongodb.net/Stats?retryWrites=true&w=majority'
 }
